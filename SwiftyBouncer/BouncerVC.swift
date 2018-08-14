2284032d60bd0fd816374a2ff83c4306c59dcc05
Robert Sellers <rsellers@Roberts-iMac.local>
2018-08-14 16:46:50 -0400

    initial commit

---
+++ SwiftyBouncer/BouncerVC.swift
@@ -0,0 +1,25 @@
+//
+//  BouncerVC.swift
+//  SwiftyBouncer
+//
+//  Created by Robert Sellers on 8/14/18.
+//  Copyright © 2018 Robert Sellers. All rights reserved.
+//
+
+import UIKit
+
+class BouncerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftyBouncer/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  SwiftyBouncer
+//
+//  Created by Robert Sellers on 8/14/18.
+//  Copyright © 2018 Robert Sellers. All rights reserved.
+//
+
+import UIKit
+
+let snapDamping: CGFloat = 0.5
+let anchorFrequency: CGFloat = 0.8
+let distanceForNewBlock: CGFloat = 110.0
+let speedDifferentialOfDestruction: CGFloat = 250.0
+let TimeDifferentialOfDestruction: CGFloat = 2.0
+let smallBlockDensity: CGFloat = 3.0
+let elasticity: CGFloat = 0.5
+
+let blockSize = CGSize(width: 60.0, height: 60.0)
+let initalCenter = CGPoint(x: 75.0, y: 80.0)
